3f8f8a6a434cfbaf02294f1299278996d09024d4
SelimYuksel <yukselselim26@gmail.com>
2024-12-27 14:56:00 +0300

    Modify script.js: added default grid generator Modify styles.css: designed the default grid layout

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 const mainContainer = document.querySelector('#container');
 const body = document.querySelector('body');
+
+const gridDefault = () => {
+    for(i = 0; i < 16; i++) {
+        for(j = 0; j < 16; j++) {
+            const row = document.createElement("div");
+            row.classList.add('grid-div');
+            mainContainer.appendChild(row);
+        }
+    }
+}
+
+gridDefault();
